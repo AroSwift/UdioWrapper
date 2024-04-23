6bdb697ef13bb0c072e2c102fea2bfef1d88bd4f
Pikachubolk <58044069+Pikachubolk@users.noreply.github.com>
2024-04-23 13:31:44 +0200

    Update __init__.py

--- udio_wrapper/__init__.py
+++ udio_wrapper/__init__.py
@@ -5,6 +5,7 @@ import nopecha
 
 class UdioWrapper:
     API_BASE_URL = "https://www.udio.com/api"
+    nopecha.api_key = nopecha_api_key
 
     def __init__(self, auth_token, nopecha_api_key):
         self.auth_token = auth_token
