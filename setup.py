368b814eb184191e7f48fe50c99ec2e8d8fd1fbd
Pikachubolk <58044069+Pikachubolk@users.noreply.github.com>
2024-04-25 21:34:48 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt", "r") as f:
 
 setup(
     name='udio_wrapper',
-    version='0.0.12',
+    version='0.0.13',
     description='Generates songs using the Udio API using textual prompts.',
     author='Flowese',
     packages=find_packages(),
